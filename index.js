86868fb9a4d41ab45af4b352ec8cabd1a692b6ec
chris-wamae <chriswamae123@gmail.com>
2022-11-25 11:53:09 +0300

    Created a function inputScore that takes in the users score as the only argument

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Challenge 1: Student Grade Generator
+// input = 0 - 100
+//Grade = A > 79,
+// B - 60 to 79,
+// C -  59 to 49,
+// D - 40 to 49,
+// E - less 40,
+
+function inputScore(studentScore)
+{
+if(studentScore >= 0 && studentScore <= 100 && studentScore >= 79)
+{console.log("GradeA")}
+}
